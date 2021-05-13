832022299fd6255fc243a090b25b22c0e97c241e
kiranpranay <kiranpranay12@gmail.com>
2021-05-13 18:55:59 +0530

    nav fi

--- assets/js/gallary.js
+++ assets/js/gallary.js
@@ -15,13 +15,47 @@ function setItemRatio() {
 }
 
 (function ($) {
-  "use strict";
+  ("use strict");
 
   // Window Resize Mobile Menu Fix
   mobileNav();
 
   // Scroll animation init
   window.sr = new scrollReveal();
+
+  // Menu Dropdown Toggle
+  if ($(".menu-trigger").length) {
+    $(".menu-trigger").on("click", function () {
+      $(this).toggleClass("active");
+      $(".header-area .nav").slideToggle(200);
+    });
+  }
+
+  // Menu elevator animation
+  $("a[href*=\\#]:not([href=\\#])").on("click", function () {
+    if (
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash);
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+      if (target.length) {
+        var width = $(window).width();
+        if (width < 991) {
+          $(".menu-trigger").removeClass("active");
+          $(".header-area .nav").slideUp(200);
+        }
+        $("html,body").animate(
+          {
+            scrollTop: target.offset().top - 130,
+          },
+          700
+        );
+        return false;
+      }
+    }
+  });
 
   // Page loading animation
   $(window).on("load", function () {
